b66d846ba315cf0ed16062fec2154d2234a534fe
quorum <micjph@protonmail.com>
2022-02-14 11:18:45 +0100

    Configure F5 for VS Code

--- firmware/kernal/r.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-./b.sh
-cd _image_
-../../../hc800-emu-ml/_build/default/hc800emulator.exe -s boot.bin ../../data/font.bin
-cd ..
--- firmware/r.sh
+++ firmware/r.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./b.sh
 cd _image_
-../../../hc800-emu-ml/_build/default/hc800emulator.exe boot.bin ../../data/font.bin
+../../../hc800-emu-ml/_build/default/hc800emulator.exe boot.bin ../../data/font.bin -s 2
 cd ..
